1380d7f730630126647754bb7a990112a5ac92ae
Gleb <glebimptab@gmail.com>
2017-09-17 22:33:19 +0300

    TaskGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.result;
+    }
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+     this.result = Math.round ((this.min + this.max)/2);
+     return this.result;
     }
 
     lower() {
-
+      return this.max = this.result;
     }
 
     greater() {
-
+            return this.min = this.result; 
     }
 }
 
